983c15fb32d618c49370aa9be0d19a85fdb5cb76
Celine <celine.dupuis@alumni.epfl.ch>
2020-05-05 14:53:00 +0200

    Update indicators while on map

--- website/javascript/interactiveSwissMap.js
+++ website/javascript/interactiveSwissMap.js
@@ -20,31 +20,46 @@ class SwissMap {
         const path = d3.geoPath()
             .projection(projection);
 
-        // Data
-        const population_promise = d3.csv("../data/swiss_indicators_2020.csv").then((data) => {
-            let cantonId_to_population = {};
+        // Data Swiss Indicators
+        const indicators_promise = d3.csv("../data/swiss_indicators_2020.csv").then((data) => {
+            let population = {};
+            let growthDomesticProductPerCapita = {};
+            let beds = {};
+            let doctors = {};
+
             data.forEach((row) => {
-                cantonId_to_population[row.id] = parseFloat(row.population);
+                population[row.id] = parseFloat(row.population);
+                growthDomesticProductPerCapita = parseFloat(row.gdp);
+                beds[row.id] = parseFloat(row.beds);
+                doctors[row.id] = parseFloat(row.doctors);
             });
-            return cantonId_to_population;
+            return { population, growthDomesticProductPerCapita, beds, doctors };
         });
 
+        // Data TopoJSON
         const map_promise = d3.json("../data/ch-cantons.json").then((topojson_raw) => {
             const canton_paths = topojson.feature(topojson_raw, topojson_raw.objects.cantons);
             return canton_paths.features;
         });
 
-        Promise.all([population_promise, map_promise]).then((results) => {
-            let cantonId_to_population = results[0];
-            let map_data = results[1];
+        // Colors
+        var red = "#F95151";
+        var lightGrey = "hsl(0, 0%, 90%)"
+        var darkGrey = "hsl(0, 0%, 40%)"
 
+        // Load Data in Promise
+        Promise.all([indicators_promise, map_promise]).then((results) => {
+            let map_data = results[1];
             map_data.map(x => {
-                x.properties.density = cantonId_to_population[x.id];
+                x.properties.density = results[0].population[x.id];
+                x.properties.gdpPerCapita = results[0].growthDomesticProductPerCapita[x.id];
+                x.properties.beds = results[0].beds[x.id];
+                x.properties.doctors = results[0].doctors[x.id];
             });
 
             // Color Scale
-            const color_scale = d3.scaleLog()
-                .range(["hsl(0, 0%, 90%)", "hsl(0, 0%, 40%)"])
+            let color_scale = d3.scaleLog()
+                .range([lightGrey, darkGrey])
                 .interpolate(d3.interpolateHcl)
                 .domain([d3.min(map_data, d => d.properties.density), d3.max(map_data, d => d.properties.density)]);
 
@@ -71,14 +86,6 @@ class SwissMap {
                 .attr("transform", d => "translate(" + path.centroid(d) + ")")
                 .text(d => d.id);
 
-            // Draw information
-            this.info_container.selectAll(".infoCanton")
-                .data(map_data)
-                .enter()
-                .append("text")
-                .classed("infoCanton", true)
-                .attr("transform", d => "translate(900, 250)")
-
             // Interaction
             d3.selectAll(".canton")
                 .on("mouseover", mouseover)
@@ -88,53 +95,76 @@ class SwissMap {
             let cantonSelected = false;
             let cantonSelectedID = "";
 
-            function mouseover(d, i) {
+            function mouseover(d) {
                 const cantonOver = d.id;
                 cantonSelectedID = d.id;
                 if (!cantonSelected) {
                     d3.selectAll(".canton")
                         .style("stroke-width", d => d.id == cantonOver ? 3 : 0.5)
-                        .style("fill", d => d.id == cantonOver ? "#F95151" : color_scale(d.properties.density));
-                    d3.selectAll(".infoCanton")
-                        .text(d => {
-                            if (d.id == cantonSelectedID) {
-                                return d.properties.name;
-                            } else {
-                                return "";
-                            }
-                        })
+                        .style("fill", d => d.id == cantonOver ? red : color_scale(d.properties.density));
                 }
+                updateIndicators();
             }
 
-            function mouseout(d, i) {
+            function mouseout(d) {
                 if (!cantonSelected) {
                     d3.selectAll(".canton")
                         .style("stroke-width", 0.5)
-                        .style("fill", d => color_scale(d.properties.density));
-                    d3.selectAll(".infoCanton")
-                        .text("");
+                        .style("fill", d => color_scale(d.properties.density))
                 }
             }
 
-            function click(d, i) {
+            function click(d) {
                 cantonSelected = cantonSelectedID == d.id ? !cantonSelected : true;
                 cantonSelectedID = d.id;
                 if (cantonSelected) {
                     d3.selectAll(".canton")
                         .style("stroke-width", d => d.id == cantonSelectedID ? 3 : 0.5)
-                        .style("fill", d => d.id == cantonSelectedID ? "#F95151" : color_scale(d.properties.density));
-                    d3.selectAll(".infoCanton")
-                        .text(d => {
-                            if (d.id == cantonSelectedID) {
-                                return d.properties.name;
-                            } else {
-                                return "";
-                            }
-                        })
+                        .style("fill", d => d.id == cantonSelectedID ? red : color_scale(d.properties.density));
                     scrollBy(0, svg_viewbox.height / 2);
                 } else {
-                    mouseout(d, i);
+                    mouseout(d);
                 }
+            }
+
+            function updateIndicators() {
+                let name = map_data.filter(canton => canton.id == cantonSelectedID)[0].properties.name;
+                let demographic = map_data.filter(canton => canton.id == cantonSelectedID)[0].properties.density;
+                let beds = map_data.filter(canton => canton.id == cantonSelectedID)[0].properties.beds;
+                let doctors = map_data.filter(canton => canton.id == cantonSelectedID)[0].properties.doctors / 100;
+
+                demographic = Number(demographic).toFixed(0).toString() + "'000 <br> inhabitants";
+                if (demographic > 1000) {
+                    demographic = Number(demographic).toFixed(0).toString()[0] + "'" + Number(demographic).toFixed(0).toString().substr(1, 3) + "'000 <br>inhabitants";
+                }
+                beds = Number(beds).toFixed(1).toString() + " beds <br> for 1000 inhabitants";
+                doctors = Number(doctors).toFixed(1).toString() + " doctors <br> for 1000 inhabitants";
+
+                document.getElementById("indicator-name").innerHTML = name;
+                document.getElementById("indicator-demographic").innerHTML = demographic;
+                document.getElementById("indicator-beds").innerHTML = beds;
+                document.getElementById("indicator-doctors").innerHTML = doctors;
+
+                overlayIndicators();
+            }
+
+            // Change Cholorpleth
+            d3.selectAll("#bedTab")
+                .on("click", bedsCholorpleth)
+
+            d3.selectAll("#densityTab")
+                .on("click", densityCholorpleth)
+
+            function bedsCholorpleth() {
+                color_scale.domain([d3.min(map_data, d => d.properties.beds), d3.max(map_data, d => d.properties.beds)]);
+                d3.selectAll(".canton")
+                    .style("fill", (d) => color_scale(d.properties.beds));
+            }
+
+            function densityCholorpleth() {
+                color_scale.domain([d3.min(map_data, d => d.properties.density), d3.max(map_data, d => d.properties.density)]);
+                d3.selectAll(".canton")
+                    .style("fill", (d) => color_scale(d.properties.density));
             }
         });
     }
@@ -144,18 +174,10 @@ function whenDocumentLoaded(action) {
     if (document.readyState === "loading") {
         document.addEventListener("DOMContentLoaded", action);
     } else {
-        // `DOMContentLoaded` already fired
         action();
     }
 }
 
 whenDocumentLoaded(() => {
     plot_object = new SwissMap('interactiveSwissMap');
-    // plot object is global, you can inspect it in the dev-console
 });
-
-
-
-var white = "#ffffff";
-var colorIn = "#FEC9C9";
-var colorClick = "#F95151";
