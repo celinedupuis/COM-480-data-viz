fb81350ebe99fbfb049048539034bd70e3245737
Celine <celine.dupuis@alumni.epfl.ch>
2020-05-23 20:14:14 +0200

    Bar chart option normalized

--- website/javascript/barChart.js
+++ website/javascript/barChart.js
@@ -14,13 +14,13 @@ class BarChart {
 
         const width_bar = 15;
         const x_domain = [0, d3.max(data, (d, i) => i)];
-        const y_domain = [0, d3.max(data, d => d.properties.ICUafter)];
-
         const x_scale = d3.scaleLinear()
             .domain(x_domain)
             .range([0, this.chart_width]);
 
-        const y_scale = d3.scaleLinear()
+
+        let y_domain = [0, d3.max(data, d => d.properties.ICUafter)];
+        let y_scale = d3.scaleLinear()
             .domain(y_domain)
             .range([this.chart_height * 3 / 4, 0]);
 
@@ -32,7 +32,7 @@ class BarChart {
             .call(x_axis)
             .attr("transform", "translate(" + offset / 2 + "," + this.chart_height + ")");
 
-        const y_axis = d3.axisLeft()
+        let y_axis = d3.axisLeft()
             .scale(y_scale);
         this.svg.append("g")
             .call(y_axis)
@@ -51,7 +51,7 @@ class BarChart {
 
         // before covid
         this.chart_container.selectAll("bar")
-            .data(data.sort(function(a, b) { return b.properties.ICUbefore - a.properties.ICUbefore }))
+            .data(data.sort(function(a, b) { return b.properties.ICUafter - a.properties.ICUafter }))
             .enter().append("rect")
             .attr("x", (d, i) => x_scale(i))
             .attr("y", d => y_scale(d.properties.ICUbefore))
@@ -60,41 +60,59 @@ class BarChart {
             .attr("width", width_bar)
             .attr("height", d => (this.chart_height * 3 / 4) - y_scale(d.properties.ICUbefore));
 
-        /*console.log(data);
-        let d2 = data.map(x => x.properties.ICUafter / x.properties.density);
-        console.log(d2);
-        this.chart_container.selectAll("bar")
-            .data(data.sort(function(a, b) { return b.properties.ICUafter - a.properties.ICUafter }))
-            .enter().append("rect")
-            .attr("x", (d, i) => x_scale(i) + width_bar)
-            .attr("y", d => y_scale(d.properties.ICUafter))
-            .attr("fill", red)
-            .attr("opacity", 1)
-            .attr("width", width_bar)
-            .attr("height", d => (this.chart_height * 3 / 4) - y_scale(d.properties.ICUafter));
-
-        // before covid
-        this.chart_container.selectAll("bar")
-            .data(data.sort(function(a, b) { return b.properties.ICUbefore - a.properties.ICUbefore }))
-            .enter().append("rect")
-            .attr("x", (d, i) => x_scale(i))
-            .attr("y", d => y_scale(d.properties.ICUbefore))
-            .attr("fill", blue)
-            .attr("opacity", 1)
-            .attr("width", width_bar)
-            .attr("height", d => (this.chart_height * 3 / 4) - y_scale(d.properties.ICUbefore));*/
-
         // canton label
         this.chart_container.append('g')
             .selectAll('text')
             .data(data.sort(function(a, b) { return b.properties.ICUafter - a.properties.ICUafter }))
             .enter()
             .append("text")
-            .classed("label", true)
+            .classed("label-chart", true)
             .text(d => d.id)
             .attr("x", (d, i) => x_scale(i) + width_bar / 2)
             .attr("y", (this.chart_height * 3 / 4) + 30)
+
+
+        // Interaction Bubble Chart Radius
+        d3.selectAll("#test")
+            .on("click", function() {
+                normalizedChart();
+            })
+
+        function normalizedChart(d) {
+            console.log("hello")
+                /*y_domain = [0, 0.5];
+                y_scale = d3.scaleLinear()
+                    .domain(y_domain)
+                    .range([barChart.chart_height * 3 / 4, 0]);
+
+                d3.selectAll("bar")
+                    .data(data.sort(function(a, b) { return (b.properties.ICUafter / b.properties.density) - (a.properties.ICUafter / a.properties.density) }))
+                    .attr("x", (d, i) => x_scale(i) + width_bar)
+                    .attr("y", d => y_scale(d.properties.ICUafter / d.properties.density))
+                    .attr("fill", red)
+                    .attr("opacity", 1)
+                    .attr("width", width_bar)
+                    .attr("height", d => (barChart.chart_height * 3 / 4) - y_scale(d.properties.ICUafter / d.properties.density));
+
+                //before
+                d3.selectAll("bar")
+                    .data(data.sort(function(a, b) { return (b.properties.ICUafter / b.properties.density) - (a.properties.ICUafter / a.properties.density) }))
+                    .enter().append("rect")
+                    .attr("x", (d, i) => x_scale(i))
+                    .attr("y", d => y_scale(d.properties.ICUbefore / d.properties.density))
+                    .attr("fill", blue)
+                    .attr("opacity", 1)
+                    .attr("width", width_bar)
+                    .attr("height", d => (this.chart_height * 3 / 4) - y_scale(d.properties.ICUbefore / d.properties.density));
+
+                d3.selectAll("label-chart")
+                    .data(data.sort(function(a, b) { return (b.properties.ICUafter / b.properties.density) - (a.properties.ICUafter / a.properties.density) }))
+                    .enter()
+                    .append("text")
+                    .classed("label", true)
+                    .text(d => d.id)
+                    .attr("x", (d, i) => x_scale(i) + width_bar / 2)
+                    .attr("y", (this.chart_height * 3 / 4) + 30)*/
+        }
     }
-
-
 };
